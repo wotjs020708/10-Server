3a8fe4276dd928d6755153e7e0fb04a57460a8a6
JaeSunEo <wotjs020708@gmail.com>
2024-07-15 11:48:33 +0900

    파라미터 전달과 조건에 따른 응답

--- helloWorld/Sources/App/routes.swift
+++ helloWorld/Sources/App/routes.swift
@@ -15,4 +15,22 @@ func routes(_ app: Application) throws {
         }
         return "Hi, \(guest). greetings! Thanks for visiting us."
     }
+    
+    app.get("student", ":name") { req async throws -> String in
+            guard let studentName = req.parameters.get("name") else {
+                throw Abort(.badRequest)
+            }
+            let studentRecords =  [
+                "Peter" : 3.42,
+                "Thomas" : 2.98,
+                "Jane" : 3.91,
+                "Ryan" : 4.00,
+                "Kyle" : 4.00
+            ]
+            if let gpa = studentRecords[studentName] {
+                return "The student \(studentName)'s GPA is \(gpa)"
+            } else {
+                return "The student's record can't be found!"
+            }
+        }
 }
