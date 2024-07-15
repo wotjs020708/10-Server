8667aa0e17433e20a32e67b0c64cc5f5675b54b3
JaeSunEo <wotjs020708@gmail.com>
2024-07-15 11:26:42 +0900

    파라미터 전달 라우트 greet 추가

--- helloWorld/Sources/App/routes.swift
+++ helloWorld/Sources/App/routes.swift
@@ -8,4 +8,11 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("greet", ":name") { req async -> String in
+        guard let guest = req.parameters.get("name") else {
+            return "Hi!"
+        }
+        return "Hi, \(guest). greetings! Thanks for visiting us."
+    }
 }
