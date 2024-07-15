2c5ec52c62d4c8c0dcc848b240091e8dd77deb32
JaeSunEo <wotjs020708@gmail.com>
2024-07-15 13:45:53 +0900

    student 에 대한 테스트 코드 작성

--- helloWorld/Package.swift
+++ helloWorld/Package.swift
@@ -4,13 +4,15 @@ import PackageDescription
 let package = Package(
     name: "helloWorld",
     platforms: [
-       .macOS(.v13)
+        .macOS(.v13)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        // MARK: add this line : numeric version error fix
+        .package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0"),
     ],
     targets: [
         .executableTarget(
@@ -19,6 +21,8 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
+                // MARK: add this line : numeric version error fix : target added
+                .product(name: "Numerics", package: "swift-numerics"),
             ],
             swiftSettings: swiftSettings
         ),
--- helloWorld/Tests/AppTests/AppTests.swift
+++ helloWorld/Tests/AppTests/AppTests.swift
@@ -20,4 +20,18 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
     }
+    
+    func testStudentRoute() async throws {
+        // Arrange
+        let studentRecords = ["Peter": 3.42, "Thomas": 2.98, "Jane": 3.91, "Ryan": 4.00, "Kyle": 4.00]
+        
+        for (studentName, gpa) in studentRecords {
+            // Act
+            try await app.test(.GET, "student/\(studentName)") { res async in
+                // Assert
+                XCTAssertEqual(res.status, .ok)
+                XCTAssertEqual(res.body.string, "The student \(studentName)'s GPA is \(gpa)")
+            }
+        }
+    }
 }
