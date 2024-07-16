2b144631a82ba617a7c0ba8acf5a81e78b338695
JaeSunEo <wotjs020708@gmail.com>
2024-07-16 17:00:22 +0900

    login 페이지 추가, admin유저 생성 마이그레이션 파일 추가

--- helloWorld/Sources/App/configure.swift
+++ helloWorld/Sources/App/configure.swift
@@ -13,6 +13,7 @@ public func configure(_ app: Application) async throws {
     // 마이그레이션 코드 추가
     app.migrations.add(CreateEntry())
     app.migrations.add(CreateAdmin())
+    app.migrations.add(CreateAdminUser())
     
     // 템플릿 엔진 leaf 추가
     app.views.use(.leaf)
