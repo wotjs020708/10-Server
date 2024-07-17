f7a2e561a9f432ff542d596d4a174283ec2ebcfc
JaeSunEo <wotjs020708@gmail.com>
2024-07-17 10:13:14 +0900

    API작성

--- helloWorld/Sources/App/configure.swift
+++ helloWorld/Sources/App/configure.swift
@@ -10,16 +10,16 @@ public func configure(_ app: Application) async throws {
     
     // Add HMAC with SHA-256 signer.
     
-
+    
     // 데이터베이스 드라이버 로드
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
     app.jwt.signers.use(.hs256(key: "your-secret-key"))  // 실제 운영 환경에서는 환경 변수 등을 통해 안전하게 관리해야 합니다.
     
     
     // // 세션을 데이터베이스에서 관리하도록 설정
-       app.sessions.use(.fluent)
-       app.migrations.add(SessionRecord.migration)
-
+    app.sessions.use(.fluent)
+    app.migrations.add(SessionRecord.migration)
+    
     
     // 마이그레이션 코드 추가
     app.migrations.add(CreateEntry())
@@ -34,6 +34,9 @@ public func configure(_ app: Application) async throws {
     
     // Journal controller 라우터 등록
     try app.register(collection: JournalController())
+    
+    // API 라우터 등록
+    try app.register(collection: APIRoutes(app: app))
     
     // register routes
     try routes(app)
