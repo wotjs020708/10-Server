d04b5eb1268a7bdaedb2b8bcfc7ae9aac35d9b54
JaeSunEo <wotjs020708@gmail.com>
2024-07-18 09:27:01 +0900

    Initialize Project VaporEntryApp

---
+++ VaporEntryApp/VaporEntryApp/VaporEntryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaporEntryAppApp.swift
+//  VaporEntryApp
+//
+//  Created by 어재선 on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct VaporEntryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
